d283e19a4ed71997d5583781bd96cfc06d86e2c5
shettysinchana <shettysinchana26@gmail.com>
2026-04-17 15:13:25 +0530

    first commit

---
+++ plant.java
@@ -0,0 +1,27 @@
+class Plant {
+    void Rose() {
+        System.out.println("this is rose");
+    }
+}
+
+class Leaf extends Plant {
+    void Lilly() {
+        System.out.println("this is lilly");
+    }
+}
+
+class Stem extends Leaf {
+    void Lotus() {
+        System.out.println("this is lotus");
+    }
+}
+
+public class Plants {
+    public static void main(String args[]) {
+        Stem c = new Stem();  
+        c.Rose();
+        c.Lilly();
+        c.Lotus();
+        System.out.println("done");
+    }
+}
